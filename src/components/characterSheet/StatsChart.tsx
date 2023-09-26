443765e3c369d793d81bc6870bb41030b3646dbd
ivanloy <ignacio.vanloy@minzehealth.com>
2023-09-26 16:59:12 +0200

    stats for calculator

--- src/components/characterSheet/StatsChart.tsx
+++ src/components/characterSheet/StatsChart.tsx
@@ -7,6 +7,21 @@ type StatsChartProps = {
   control: any;
 };
 
+
+const getXpForLevel = (level: number) => {
+  const pointsPerLevel = [ 90, 135, 180, 255, 330, 435, 545, 665, 800, 950, 1100, 1250, 1350, 1515, 1680,
+    1830, 2010, 2200, 2410, 2580, 2820, 3130, 3330, 3600, 3920, 4265, 4610, 5000, 5390, 5840, 6320, 6800, 7400, 8000 ];
+
+  return pointsPerLevel[Math.floor(level - 1 / 3)];
+}
+
+const getStatCost = (rank: number) => {
+  const pointsPerRank = [ -100, -50, -20, 0, 10, 30, 60, 100, 150, 210, 280, 360, 450, 550, 675, 
+    825, 1000, 1200, 1450, 1750, 2200, 2850, 3750, 5000, 7000];
+
+  return pointsPerRank[rank - 1];
+}
+
 const stats = [
   {
     name: "Fuerza",
